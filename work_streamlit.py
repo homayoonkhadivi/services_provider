1a88a14180fcaac255aa5184d92ff55be5c7e619
Homayoon <57557590+homayoonkhadivi@users.noreply.github.com>
2024-07-21 13:42:58 +0930

    Update work_streamlit.py

--- work_streamlit.py
+++ work_streamlit.py
@@ -177,14 +177,16 @@ elif option == 'Visa & Assessment Advice':
             st.image(visa_cdr_image, caption='Learn Australia Visa & Assessment', use_column_width=True)
         with col2:
             st.write("""
-            I provide advice on Australian visas and assessments, covering a wide range of visa conditions and assessment requirements.
-                
-            **Topics Covered:**
-            - Advice on PTE & IELTS English Exams
-            - Obtaining certificate assessments from various Australian organizations, such as Engineers Australia
-            - Over 50 positive CDR assessments without comments in all engineering fields
-            - Guidance on selecting the most suitable visa based on your individual circumstances
-            """)
+            Looking to migrate to Australia or obtain professional assessment for your engineering qualifications? As a professional advisor, I can guide you through the visa and assessment process, ensuring a smooth and successful outcome.
+
+    **Topics Covered:**
+    - Advice on PTE & IELTS English Exams
+    - Obtaining certificate assessments from various Australian organizations, such as Engineers Australia, TRA (Trades Recognition Australia), WETassess, etc.
+    - Over 50 successful CDR assessments in all engineering fields with no comments
+    - Guidance on selecting the most suitable visa based on your individual circumstances
+
+    With expertise and experience in the field, I can address all your concerns and guide you through the complexities of the visa and assessment process. Whether you're an engineer seeking recognition or an individual looking to migrate to Australia, I can offer personalized advice to ensure we achieve your desired outcomes.
+    """)
 elif option == 'Tax Return Advice':
     st.header("Tax Return Advice")
     st.write("""

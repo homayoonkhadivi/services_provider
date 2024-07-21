eac7d329e7603502e5a43ff717db0c474f8683c2
Homayoon <57557590+homayoonkhadivi@users.noreply.github.com>
2024-07-21 13:23:26 +0930

    Update work_streamlit.py

--- work_streamlit.py
+++ work_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import os
 
-# Directory path
+# Directory path for images
 image_dir = './images'
 
 # Function to load images safely
@@ -21,7 +21,7 @@ visa_cdr_image_path = os.path.join(image_dir, 'visa_cdr.jpg')
 # Load images
 swimming_image = load_image(swimming_image_path)
 datascience_image = load_image(datascience_image_path)
-visacdr_image = load_image(visa_cdr_image_path)
+visa_cdr_image = load_image(visa_cdr_image_path)
 
 # Recommended books and podcasts for personal growth
 recommended_books = [
@@ -76,21 +76,23 @@ if option == 'Swimming Lessons':
         with col2:
             st.write("""
             ### Introduction:
-                    "I am a professional swimmer, swim coach, international lifeguard, and water therapist. Having been a member of Iran's National Swimming Team for 8 years and holding two national records in backstroke for a significant period,
-                     I offer comprehensive swimming lessons for students of all skill levels.
+            I am a professional swimmer, swim coach, international lifeguard, and water therapist. Having been a member of Iran's National Swimming Team for 8 years and holding two national records in backstroke for a significant period,
+            I offer comprehensive swimming lessons for students of all skill levels.
 
-             #### Services Offered:
-                - Swimming Lessons
+            #### Services Offered:
+            - Swimming Lessons
 
-            **Topics Covered**
-                - Basic swimming techniques
-                - Advanced strokes
-                - Breathing techniques
-                - Safety tips
-            **Hydrotherapy Sessions**
-                - **Benefits:**
+            **Topics Covered:**
+            - Basic swimming techniques
+            - Advanced strokes
+            - Breathing techniques
+            - Safety tips
+
+            **Hydrotherapy Sessions:**
+            - **Benefits:**
             Treat aches and pains such as headaches, muscle pain, migraines, and toothaches
-            Contact Information:
+
+            **Contact Information:**
             Feel free to reach out if you have any questions or need more information!
             """)
 elif option == 'Data Science Teaching':
@@ -101,63 +103,63 @@ elif option == 'Data Science Teaching':
             st.image(datascience_image, caption='Learn Data Science', use_column_width=True)
         with col2:
             st.write("""
-                I provide data science teaching that covers a wide range of topics, from the basics to advanced machine learning techniques. If you are looking to start a career in data science or enhance your skills, I can guide you through the process.
-                I can teach you in different programming languages, software tools, and ML/ AI Concepts such as:
+            I provide data science teaching that covers a wide range of topics, from the basics to advanced machine learning techniques. If you are looking to start a career in data science or enhance your skills, I can guide you through the process.
+            I can teach you in different programming languages, software tools, and ML/AI concepts such as:
 
-                - Cloud Technology: AWS (SageMaker Studio, AI Services)
-                - Programming languages and libraries: Python (Pandas, Numpy, Matplotlib, Plotly, Seaborn, Sci-Kit Learn), R Studio, PostgreSQL, MySQL, Docker, SQLAlchemy, Tensorflow, Keras, Pytorch, Object Oriented Programming (OOP), HTML, CSS, StreamLit, FastAPI, MLFlow, CML CI/CD 
-                - Tools: Microsoft Power BI, Tableau, SSIS, ETL Processes, , MS Office Suite
-                - Computer Vision: CNN & Transfer Learning to apply Artificial Intelligence in Medical Diagnosis
-                - Natural Language Processing NLP: Natural Language Understanding, Language Models like BERT, T5, GPT-2and GPT-3, Named Entity Recognition, Topic Modelling, Sentiment Analysis, Document Similarity, Text Classification, Text Summarization
-                - Statistical Analysis: Bayesian Analysis, A/B testing, Regression, Time Series, Hypothesis testing, Sampling, T-Test, ANOVA
-                
-                **Teaching Topics:**
-                - Introduction to Data Science
-                - Data Analysis with Python, SQL
-                - Machine Learning
-                - Data Visualization
-                - Deep Learning
-                - Machine Learning on Production
-                - MLOps Concepts
-                - Deploy ML Models
+            - Cloud Technology: AWS (SageMaker Studio, AI Services)
+            - Programming languages and libraries: Python (Pandas, Numpy, Matplotlib, Plotly, Seaborn, Sci-Kit Learn), R Studio, PostgreSQL, MySQL, Docker, SQLAlchemy, TensorFlow, Keras, PyTorch, Object-Oriented Programming (OOP), HTML, CSS, Streamlit, FastAPI, MLFlow, CML CI/CD
+            - Tools: Microsoft Power BI, Tableau, SSIS, ETL Processes, MS Office Suite
+            - Computer Vision: CNN & Transfer Learning to apply Artificial Intelligence in Medical Diagnosis
+            - Natural Language Processing (NLP): Natural Language Understanding, Language Models like BERT, T5, GPT-2, and GPT-3, Named Entity Recognition, Topic Modelling, Sentiment Analysis, Document Similarity, Text Classification, Text Summarization
+            - Statistical Analysis: Bayesian Analysis, A/B Testing, Regression, Time Series, Hypothesis Testing, Sampling, T-Test, ANOVA
 
-                **About Me:**
-                - [Kaggle Expert](https://www.kaggle.com/homayoonkhadivi) in notebooks and datasets
-                - [Professional AI/ML Scientist](https://www.linkedin.com/in/homayoon-khadivi/)
+            **Teaching Topics:**
+            - Introduction to Data Science
+            - Data Analysis with Python, SQL
+            - Machine Learning
+            - Data Visualization
+            - Deep Learning
+            - Machine Learning in Production
+            - MLOps Concepts
+            - Deploy ML Models
+
+            **About Me:**
+            - [Kaggle Expert](https://www.kaggle.com/homayoonkhadivi) in notebooks and datasets
+            - [Professional AI/ML Scientist](https://www.linkedin.com/in/homayoon-khadivi/)
             """)
 elif option == 'Visa & Assessment Advice':
     st.header("Australia Visa & Assessment Advice")
-    if visacdr_image:
+    if visa_cdr_image:
         col1, col2 = st.columns([1, 3])
         with col1:
-            st.image(visacdr_image, caption='Learn Australia Visa & Assessment', use_column_width=True)
+            st.image(visa_cdr_image, caption='Learn Australia Visa & Assessment', use_column_width=True)
         with col2:
             st.write("""
-                I provide advice on Australian visas and assessments, covering a wide range of visa conditions and assessment requirements.
+            I provide advice on Australian visas and assessments, covering a wide range of visa conditions and assessment requirements.
                 
-                **Topics Covered:**
-                - Advice on PTE & IELTS English Exams
-                - Obtaining certificate assessments from various Australian organizations, such as Engineers Australia
-                - Over 50 positive CDR assessments without comments in all engineering fields
-                - Guidance on selecting the most suitable visa based on your individual circumstances
+            **Topics Covered:**
+            - Advice on PTE & IELTS English Exams
+            - Obtaining certificate assessments from various Australian organizations, such as Engineers Australia
+            - Over 50 positive CDR assessments without comments in all engineering fields
+            - Guidance on selecting the most suitable visa based on your individual circumstances
             """)
 elif option == 'Tax Return Advice':
     st.header("Tax Return Advice")
     st.write("""
-        I offer comprehensive tax return advice to help you maximize your tax returns and optimize your financial situation.
+    I offer comprehensive tax return advice to help you maximize your tax returns and optimize your financial situation.
         
-        **Services Offered:**
-        - Filing your tax return accurately and on time
-        - Maximizing deductions and credits to ensure you get the highest possible return
-        - Providing advice on tax planning strategies to minimize your tax liabilities
-        - Guidance on how to handle specific tax situations and requirements
+    **Services Offered:**
+    - Filing your tax return accurately and on time
+    - Maximizing deductions and credits to ensure you get the highest possible return
+    - Providing advice on tax planning strategies to minimize your tax liabilities
+    - Guidance on how to handle specific tax situations and requirements
         
-        Whether you're an individual or a small business owner, I can assist you in navigating the complexities of tax regulations and ensuring compliance while optimizing your financial outcomes.
+    Whether you're an individual or a small business owner, I can assist you in navigating the complexities of tax regulations and ensuring compliance while optimizing your financial outcomes.
     """)
 elif option == 'Books & Podcasts for Personal Growth':
     st.header("Books & Podcasts for Personal Growth")
     st.write("""
-        Explore recommended books and podcasts to enhance your personal growth journey.
+    Explore recommended books and podcasts to enhance your personal growth journey.
     """)
     
     # Display recommended books
@@ -176,5 +178,5 @@ elif option == 'Books & Podcasts for Personal Growth':
 
 # Footer
 st.write("""
-    Feel free to reach out if you have any questions or need more information!
+Feel free to reach out if you have any questions or need more information!
 """)

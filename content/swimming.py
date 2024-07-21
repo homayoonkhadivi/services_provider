583f8fc91ce28efffc6f4584687f7b302bb9c93e
Homayoon <57557590+homayoonkhadivi@users.noreply.github.com>
2024-07-21 14:32:11 +0930

    Update swimming.py

--- content/swimming.py
+++ content/swimming.py
@@ -9,7 +9,7 @@ def display_swimming():
         col1, col2 = st.columns([1, 3])
         with col1:
             st.image(swimming_image, caption='Learn to Swim', use_column_width=True)
-        with col2:
+         with col2:
             st.write("""
             ### Introduction:
             I am a professional swimmer, swim coach, international lifeguard, and water therapist. Having been a member of Iran's National Swimming Team for 8 years and holding two national records in backstroke for a significant period,
